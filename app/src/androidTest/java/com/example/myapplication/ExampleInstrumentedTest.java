8ef20030f6ae2867c769046233049bf8b256e3bc
Martin Winandy <martin.winandy@pmwmedia.de>
2022-02-06 22:13:39 +0100

    Provide passed, skipped, and failed test method

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,12 +1,25 @@
 package com.example.myapplication;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void passedTest() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+
+    @Test
+    public void failedTest() {
         assertThat(1 + 1).isEqualTo(3);
+    }
+
+    @Test
+    @Ignore
+    public void skippedTest() {
+        fail("Should be skipped");
     }
 }
